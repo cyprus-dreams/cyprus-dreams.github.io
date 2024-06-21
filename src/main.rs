457423df0271530ff6c4b24664eaf4ebcce6298b
zombkit <stephen.korzen@gmail.com>
2024-06-21 12:50:01 -0700

    s

--- src/main.rs
+++ src/main.rs
@@ -101,40 +101,36 @@ impl Default for Masterik {
 fn keyboard_input_system(
     input: Res<ButtonInput<KeyCode>>,
     mut masterok: ResMut<Masterik>,
+    mut query_camera: Query<&mut OrthographicProjection, With<Camera>>,
   
 ) {
     let char_up = input.any_pressed([KeyCode::KeyW]);
     let char_down = input.any_pressed([KeyCode::KeyS]);
     let char_left = input.any_pressed([KeyCode::KeyA]);
     let char_right = input.any_pressed([KeyCode::KeyD]);
+    let char_q = input.any_just_pressed([KeyCode::KeyQ]); //zoom out
+    let char_e = input.any_just_pressed([KeyCode::KeyE]); //zoom in
 
-    let cursor_up = input.any_just_pressed([KeyCode::KeyI]);
-    let cursor_down = input.any_just_pressed([KeyCode::KeyK]);
-    let cursor_left = input.any_just_pressed([KeyCode::KeyJ]);
-    let cursor_right = input.any_just_pressed([KeyCode::KeyL]);
+    let mut projection = query_camera.single_mut();
+ 
+  
+    if char_q {
+          // zoom out
+    projection.scale *= 1.25;
+    }
+    if char_e {
+           // zoom in
+    projection.scale /= 1.25;
+    }
 
-    let char_attack = input.any_just_pressed([KeyCode::KeyY]); // jęti (jme) / vzeti
-    let char_take = input.any_just_pressed([KeyCode::KeyG]); // jęti (jme) / vzeti metnuti  imej target range do ktorogo mozno metati dla praktiki zeby povysati skil be ubijstva
-    let char_drop = input.any_just_pressed([KeyCode::KeyB]); //izbaviti se
-    let char_help = input.any_just_pressed([KeyCode::KeyT]); //pokazati pomoc ?
-
-    let char_one = input.any_just_pressed([KeyCode::Digit1]);
-    let char_two = input.any_just_pressed([KeyCode::Digit2]);
-    let char_three = input.any_just_pressed([KeyCode::Digit3]);
-    let char_four = input.any_just_pressed([KeyCode::Digit4]);
-    let char_five = input.any_just_pressed([KeyCode::Digit5]);
-    let char_six = input.any_just_pressed([KeyCode::Digit6]);
-    let char_seven = input.any_just_pressed([KeyCode::Digit7]);
-    let char_eight = input.any_just_pressed([KeyCode::Digit8]);
-    let char_nine = input.any_just_pressed([KeyCode::Digit9]);
-    let char_zero = input.any_just_pressed([KeyCode::Digit0]);
+ 
 
     let char_backspace = input.any_pressed([KeyCode::Backspace, KeyCode::Delete]);
-    let char_quit = input.any_just_pressed([KeyCode::KeyQ]);
+ 
 
    
 
-    if char_quit {
+    if char_backspace {
         panic!("BYE");
     }
   
@@ -206,3 +202,4 @@ impl Default for BevyTerminal<RataguiBackend> {
         }
     }
 }
+
