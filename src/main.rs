a151f6a85ee49e7327fb120337dbbf4c633472ff
zombkit <stephen.korzen@gmail.com>
2024-06-24 01:58:28 -0700

    asd

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
                     // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
                     prevent_default_event_handling: false,
                     window_theme: Some(WindowTheme::Dark),
-                    canvas: Some(String::from("#kosmos")),
+                    canvas: Some(String::from("kosmos")),
                   
             
                     ..default()
